a5ae16fffcf9738a8c4bef6f75d16a2fb4cecbf9
OvejaBasualdo <elportenio@gmail.com>
2023-01-23 17:14:25 -0300

    Adding files

---
+++ src/main/java/com/accenture/paymentModule/dto/UserDTO.java
@@ -0,0 +1,77 @@
+package com.accenture.paymentModule.dto;
+
+import com.accenture.paymentModule.entity.User;
+
+public class UserDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String dni;
+    private String email;
+    private String password;
+
+    public UserDTO(User user) {
+        this.id = user.getId();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.dni = user.getDni();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+    }
+
+    public UserDTO(String firstName, String lastName, String dni, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dni = dni;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/accenture/paymentModule/models/Account.java
@@ -0,0 +1,64 @@
+package com.accenture.paymentModule.models;
+
+import com.accenture.paymentModule.entity.User;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Account {
+    private Long id;
+    private String accountNumber;
+    private String cbu;
+    private BigDecimal balance;
+    private LocalDateTime creationDate;
+
+    private Long userId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getCbu() {
+        return cbu;
+    }
+
+    public void setCbu(String cbu) {
+        this.cbu = cbu;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
