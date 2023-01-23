d3ffab9eefcb0d9fda91ce34e86e4f5d5193f2a1
OvejaBasualdo <elportenio@gmail.com>
2023-01-23 17:12:35 -0300

    Adding methods in service

--- src/main/java/com/accenture/paymentModule/service/IUserService.java
+++ src/main/java/com/accenture/paymentModule/service/IUserService.java
@@ -3,6 +3,8 @@ package com.accenture.paymentModule.service;
 import com.accenture.paymentModule.entity.User;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 public interface IUserService {
     public List<User> findAll();
@@ -10,5 +12,6 @@ public interface IUserService {
     public User findByFirstNameIgnoreCase(String firstName);
     public User findByLastNameIgnoreCase(String lastName);
     public User findByDni (String dni);
+    public Long findByAccountId(Long accountId);
 
 }
--- src/main/java/com/accenture/paymentModule/service/UserServiceImpl.java
+++ src/main/java/com/accenture/paymentModule/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.accenture.paymentModule.service;
 
+import com.accenture.paymentModule.dto.UserDTO;
 import com.accenture.paymentModule.entity.User;
 import com.accenture.paymentModule.models.Account;
 import com.accenture.paymentModule.repository.UserRepository;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
+import java.util.*;
 
 @Service
 public class UserServiceImpl implements IUserService {
@@ -31,9 +32,10 @@ public class UserServiceImpl implements IUserService {
 
     @Override
     @Transactional(readOnly = true)
-    public User findByFirstNameIgnoreCase(String firstName){
+    public User findByFirstNameIgnoreCase(String firstName) {
         return userRepository.findByFirstNameIgnoreCase(firstName).orElse(null);
     }
+
     @Override
     @Transactional(readOnly = true)
     public User findByLastNameIgnoreCase(String lastName) {
@@ -44,5 +46,47 @@ public class UserServiceImpl implements IUserService {
     @Transactional(readOnly = true)
     public User findByDni(String dni) {
         return userRepository.findByDni(dni).orElse(null);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Long findByAccountId(Long accountId) {
+        return userRepository.findByAccountId(accountId).orElse(null);
+    }
+
+    public List<Long> findAccountFromUserId(Long userId) {
+        Map<String, String> pathVariables = new HashMap<>();
+        pathVariables.put("userId", userId.toString());
+        List<Long> accountsId = Arrays.stream(accountRest.getForObject("http://localhost:8002/api/accounts/list/userId/{userId}", Long[].class, pathVariables)).toList();
+        return accountsId;
+    }
+
+    public void createUser(UserDTO userDTO) {
+        User userWithoutAccount = new User(userDTO);
+        userRepository.save(userWithoutAccount);
+        User user = userRepository.findByDni(userWithoutAccount.getDni()).orElse(null);
+        accountRest.postForEntity("http://localhost:8002/api/accounts/createAccount", user, Account.class);
+        Map<String, String> pathVariables = new HashMap<>();
+        pathVariables.put("userId", user.getId().toString());
+        List<Account> accounts = Arrays.stream(accountRest.getForObject("http://localhost:8002/api/accounts/list/userId/{userId}", Account[].class, pathVariables)).toList();
+        Set<Long> accountsId = new HashSet<>();
+        for (Account aux : accounts) {
+            accountsId.add(aux.getId());
+        }
+        user.setAccountId(accountsId);
+        userRepository.save(user);
+    }
+    public void createAccountToUser(UserDTO userDTO) {
+        User user = userRepository.findByDni(userDTO.getDni()).orElse(null);
+        accountRest.postForEntity("http://localhost:8002/api/accounts/createAccount", user, Account.class);
+        Map<String, String> pathVariables = new HashMap<>();
+        pathVariables.put("userId", user.getId().toString());
+        List<Account> accounts = Arrays.stream(accountRest.getForObject("http://localhost:8002/api/accounts/list/userId/{userId}", Account[].class, pathVariables)).toList();
+        Set<Long> accountsId = new HashSet<>();
+        for (Account aux : accounts) {
+            accountsId.add(aux.getId());
+        }
+        user.setAccountId(accountsId);
+        userRepository.save(user);
     }
 }
