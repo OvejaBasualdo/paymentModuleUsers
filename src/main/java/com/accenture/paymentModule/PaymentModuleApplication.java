1275fb98873b0c0df40f3b2e568ddb3ac55f910d
OvejaBasualdo <elportenio@gmail.com>
2023-01-26 15:57:16 -0300

    Adding Feign configuration

--- src/main/java/com/accenture/paymentModule/PaymentModuleApplication.java
+++ src/main/java/com/accenture/paymentModule/PaymentModuleApplication.java
@@ -4,12 +4,17 @@ import com.accenture.paymentModule.entity.User;
 import com.accenture.paymentModule.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-
+@EnableFeignClients
 @SpringBootApplication
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class PaymentModuleApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentModuleApplication.class, args);
@@ -18,13 +23,13 @@ public class PaymentModuleApplication {
     @Bean
     public CommandLineRunner initData(UserRepository userRepository) {
         return (args) -> {
-
+/*
             User user = new User("Juan", "Perez", "20.876.444", "juan.perez@gmail.com", "1234");
             userRepository.save(user);
             User user1 = new User("Camila", "Rodriguez", "30.543.123", "cami.ro@gmail.com", "5678");
             userRepository.save(user1);
             User user2 = new User("Marcelo", "Alvarez", "30.451.324", "marcelo@gmail.com", "1357");
-            userRepository.save(user2);
+            userRepository.save(user2);*/
         };
     }
 }
